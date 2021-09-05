965e7c7eceabaa94fd4e20193f9a9281ff350b3d
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-09-05 17:15:36 +0300

    Stage 1 homework 2

---
+++ src/main/java/stage1/hw2/Main.java
@@ -0,0 +1,37 @@
+package stage1.hw2;
+
+import java.util.*;
+
+public class Main {
+
+    public static String getDayOfWeekName(int number) {
+        switch (number) {
+            case 1:
+                return "Mon";
+            case 2:
+                return "Tue";
+            case 3:
+                return "Wed";
+            case 4:
+                return "Thu";
+            case 5:
+                return "Fri";
+            case 6:
+                return "Sat";
+            case 7:
+                return "Sun";
+            default:
+                throw new IllegalArgumentException("Unexpected value: " + number);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dayNumber = scanner.nextInt();
+        try {
+            System.out.println(getDayOfWeekName(dayNumber));
+        } catch (Exception e) {
+            System.out.println(e.getClass().getName());
+        }
+    }
+}
